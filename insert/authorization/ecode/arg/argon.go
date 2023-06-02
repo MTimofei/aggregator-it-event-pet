9552691510263074770c9d594eb359ee0cc1086d
MTimofei <work.tim@rambler.ru>
2023-06-02 14:39:43 +0300

    create test arg

--- insert/authorization/ecode/arg/argon.go
+++ insert/authorization/ecode/arg/argon.go
@@ -1,6 +1,7 @@
 package arg
 
 import (
+	"errors"
 	"new/insert/authorization/config"
 	"new/insert/authorization/ecode"
 	"new/pkg/gen"
@@ -8,26 +9,38 @@ import (
 	"golang.org/x/crypto/argon2"
 )
 
+const (
+	ErrInValidSalt = "invalid salt"
+)
+
 // структура реализует интерфейс ECode
 type Hash struct {
-	staticSalt  []byte
-	dynamicSalt []byte
+	StaticSalt  []byte
+	DynamicSalt []byte
 }
 
 // создает структуру для новаого пользователя(регистрации)
 func New() *Hash {
-	return &Hash{staticSalt: []byte(config.StaticSalt), dynamicSalt: gen.Rand8bytes()}
+	return &Hash{StaticSalt: []byte(config.StaticSalt), DynamicSalt: gen.Rand8bytes()}
 }
 
-// создает структуру c salt (используется для идентификации)
-func Create(salt []byte) *Hash {
-	return &Hash{staticSalt: []byte(config.StaticSalt), dynamicSalt: salt}
+// создает структуру c salt, len(salt)==8 (используется для идентификации)
+// встучи не правильной длины слайса возврощает ошибку
+func Create(salt []byte) (*Hash, error) {
+
+	switch {
+	case len(salt) != 8:
+		return nil, errors.New(ErrInValidSalt)
+	default:
+		return &Hash{StaticSalt: []byte(config.StaticSalt), DynamicSalt: salt}, nil
+	}
+
 }
 
 // извекает хэшь пользователя с использованием argon2
 func (object *Hash) Hesh(password string) (h *ecode.Hash) {
-	saticPass := argon2.IDKey([]byte(password), object.staticSalt, 4, 16*1024, 4, 128)
-	pass := argon2.IDKey(saticPass, object.dynamicSalt, 4, 16*1024, 4, 128)
+	saticPass := argon2.IDKey([]byte(password), object.StaticSalt, 4, 16*1024, 4, 128)
+	pass := argon2.IDKey(saticPass, object.DynamicSalt, 4, 16*1024, 4, 128)
 
-	return ecode.New(pass, object.dynamicSalt)
+	return ecode.New(pass, object.DynamicSalt)
 }
--- insert/authorization/ecode/arg/argon_test.go
+++ insert/authorization/ecode/arg/argon_test.go
@@ -1,6 +1,8 @@
 package arg_test
 
 import (
+	"errors"
+	"new/insert/authorization/config"
 	"new/insert/authorization/ecode"
 	"new/insert/authorization/ecode/arg"
 	"reflect"
@@ -9,10 +11,71 @@ import (
 
 var _ ecode.ECode = (*arg.Hash)(nil)
 
+// тест
 func TestNew(t *testing.T) {
 	result := arg.New()
 
 	if reflect.DeepEqual(*result, arg.Hash{}) {
 		t.Error("mathed New not valid")
+	}
+}
+
+func TestCreate(t *testing.T) {
+	var result *arg.Hash
+	var err error
+
+	testCases := []struct {
+		nameTest    string
+		salt        []byte
+		expected    *arg.Hash
+		errExpected error
+	}{
+		{
+			"test1",
+			[]byte("tryu1758"),
+			&arg.Hash{[]byte(config.StaticSalt), []byte("tryu1758")},
+			nil,
+		},
+		{
+			"test2",
+			[]byte("tryu175"),
+			nil,
+			errors.New(arg.ErrInValidSalt),
+		},
+		{
+			"test3",
+			[]byte("tryu17589"),
+			nil,
+			errors.New(arg.ErrInValidSalt),
+		},
+	}
+
+	for _, tC := range testCases {
+		t.Run(tC.nameTest, func(t *testing.T) {
+			result, err = arg.Create(tC.salt)
+
+			if !reflect.DeepEqual(err, tC.errExpected) {
+				t.Errorf("\nerrExpected:\n%v\nerr:\n%v\n", tC.errExpected, err)
+				//return
+			}
+			if !reflect.DeepEqual(result, tC.expected) {
+				t.Errorf("\nexpected:\n%v\nin:\n%v\n", tC.expected, result)
+			}
+		})
+	}
+}
+
+func TestHash(t *testing.T) {
+	testCases := []struct {
+		nameTest    string
+		password    string
+		expected    *ecode.Hash
+		errExpected error
+	}{}
+
+	for _, tC := range testCases {
+		t.Run(tC.nameTest, func(t *testing.T) {
+
+		})
 	}
 }
